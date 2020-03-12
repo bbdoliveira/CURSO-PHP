b6c27eac5db2df787cc99f5f6fd2914b434f8d34
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2020-03-12 00:00:09 -0300

    Terminando os tipos Inteiros

--- tipos/int.php
+++ tipos/int.php
@@ -1,2 +1,14 @@
 <div class="titulo">Tipos Inteiro</div>
 
+<?php
+echo 11;
+echo '<br>';
+
+var_dump(11); //Função que mostra detalhes da varipavel.
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>';
+echo PHP_INT_MIN, '<br>';
+echo 017, '<br>'; // Base octal
+echo 0b11100001011101, '<br>'; // Base Binária
+echo 0x117acf0, '<br>'; // Base hexadecimal
