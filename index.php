16ade2d9d0daf84b52cfe19a938b720948885b2f
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2020-03-11 23:48:28 -0300

    Criação do Modulo dois Tipos

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01</h3>
+                    <h3>Básico</h3>
                     <ul>
                         <li>
                             <a href="exercicio.php?dir=basico&file=ola">
@@ -41,6 +41,16 @@
                         <li>
                             <a href="exercicio.php?dir=basico&file=desafio">
                                 Desafio
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3>Tipos</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=int">
+                                Tipo Inteiro
                             </a>
                         </li>
                     </ul>
