1503e05642133faed7c93d003fa1ab30fed9207c
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2020-03-12 20:26:50 -0300

    Exercicio Tipo Float

--- index.php
+++ index.php
@@ -53,6 +53,16 @@
                                 Tipo Inteiro
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=float">
+                                Tipo Float
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=aritmeticas">
+                                Op. Aritméticas
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </nav>
